92674eb5a575fa457583080e6fca1358dc890edf
Jp-moraiss <164018039+Jp-moraiss@users.noreply.github.com>
2025-09-25 10:51:53 -0300

    Fa CRUD

---
+++ src/main/java/com/cesar/superstats/dto/FaDTO.java
@@ -0,0 +1,14 @@
+package com.cesar.superstats.dto;
+
+import lombok.Data;
+
+@Data
+public class FaDTO {
+    private String email;
+    private String nome;
+    private String genero;
+    private Integer idade;
+    private String univ_fav;
+    private Integer tempoGeek;
+    private String ocupacao;
+}
