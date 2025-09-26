4b67285ac74c06bda7e1773bbb98acb877f9ae65
Jp-moraiss <164018039+Jp-moraiss@users.noreply.github.com>
2025-09-26 00:23:19 -0300

    Created specific exception to service uses

---
+++ src/main/java/com/cesar/superstats/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cesar.superstats.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
